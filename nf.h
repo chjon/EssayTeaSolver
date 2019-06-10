ad6a014802d60e31eb88bc9558e76a75cb63e858
Jonathan Chung <jt2chung@uwaterloo.ca>
2019-06-09 22:10:46 -0400

    delete old implementation

--- nf.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "dag.h"
-
-dag_node_t* nf_cnf(dag_node_t*);
-dag_node_t* nf_dnf(dag_node_t*);
-dag_node_t* nf_nnf(dag_node_t*);
--- tt.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "dag.h"
-
-/***** FUNCTIONS *****/
-
-dag_node_t* tt_transform(dag_node_t*);
